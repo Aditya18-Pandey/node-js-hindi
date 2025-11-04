b22fe6ce0bf7f1853024abf22b1a7277e6393939
Aditya Pandey <pan.aditya11.22@gmail.com>
2025-11-04 11:06:04 +0530

    data_types

---
+++ data_types.js
@@ -0,0 +1,17 @@
+// Primitive
+
+//7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 200 
+const scoreValue = 200.4
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+const id= Symbol('1234')
+const anotherId = Symbol('1234')
+
+
+//reference(Non-Primitive)
+
+//Array, Objects, Functions
