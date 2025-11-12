cacf375d60e17f890cc7ed0aada806bf861ebe7d
Aditya Pandey <pan.aditya11.22@gmail.com>
2025-11-12 10:26:40 +0530

    Control Flow

---
+++ Control_flow.js
@@ -0,0 +1,11 @@
+// Using if statements
+
+if ( 2 == "2" ) {
+    console.log("Matched");
+    
+}
+// This is for the comparison of the Data types also
+if ( 2 === "2" ) {
+    console.log("Matched");
+    
+}
